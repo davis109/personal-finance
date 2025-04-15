70d1dba7e04ef92feeab85f5817b2e2508aae782
davis109 <sebastiandavis109@gmail.com>
2025-04-15 21:58:32 +0530

    Update next.config.js for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  }
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
